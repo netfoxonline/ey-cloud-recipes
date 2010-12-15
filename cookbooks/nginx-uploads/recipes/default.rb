749a52f245c9638c09ad5c44fe83a9750be3dad4
Daniel Draper <daniel@codefire.com.au>
2010-12-15 20:56:31 +1030

    Added file for nginx-upload: Very Catapult specific now

--- cookbooks/nginx-uploads/recipes/default.rb
+++ cookbooks/nginx-uploads/recipes/default.rb
@@ -6,6 +6,13 @@ if node[:instance_role] =~ /^app/
     mode 0755
   end
 
+  remote_file "/etc/nginx/servers/keep.catapult.rewrites" do
+    source "catapult.rewrites"
+    owner "deploy"
+    group "deploy"
+    mode 0755
+  end
+
   directory '/mnt/temp_uploads/' do
     owner 'deploy'
     group 'deploy'
