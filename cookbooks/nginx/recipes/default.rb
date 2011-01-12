2b94227daa95a1898206140d0fa1c956c6f0ed27
Daniel Draper <daniel@codefire.com.au>
2011-01-12 10:54:15 +1030

    Removed nginx recipe

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,13 +1,23 @@
-enable_package "www-servers/nginx" do
-  version "0.7*"
-end
 
-package "www-servers/nginx" do
-  version "0.7.65-r2"
-  action :install
-end
+if node[:instance_role] =~ /^app/
+  enable_package "www-servers/nginx" do
+    version "0.7*"
+  end
 
-service "nginx" do
-  supports :status => true, :stop => true, :restart => true, :staus => true
-  action :restart
+  package "www-servers/nginx" do
+    version "0.7.65-r2"
+    action :install
+  end
+
+  service "nginx" do
+    supports :status => true, :stop => true, :restart => true, :staus => true
+    action :restart
+  end
+
+  remote_file "/etc/nginx/common/proxy.conf" do
+    source "common.proxy.conf"
+    owner "deploy"
+    group "deploy"
+    mode 0755
+  end
 end
