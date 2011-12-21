7938113726fbcadb4f896363078866b8fdc0c706
Daniel Draper <daniel@codefire.com.au>
2011-12-21 21:00:34 +1030

    Added collector and copier recipes

--- cookbooks/collector/recipes/default.rb
+++ cookbooks/collector/recipes/default.rb
@@ -1,14 +1,31 @@
 if node[:instance_role] =~ /^app/
-  template "/home/deploy/.pgpass" do
-    owner 'deploy'
-    group 'deploy'
-    mode 0600
-    source "pgpass.erb"
-    variables({
-      :hostname => node[:db_host],
-      :dbuser => node[:users].first[:username],
-      :dbpass => node[:users].first[:password]
-    })
+  remote_file "/engineyard/bin/collector" do
+    source "collector"
+    owner "root"
+    group "root"
+    mode 0755
+  end
+
+  node[:applications].each do |app_name,data|
+    1.times do |count| # TODO: Use multiple collectors later
+      template "/etc/monit.d/collector#{count+1}.#{app_name}.monitrc" do
+        source "collector.monitrc.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        variables({
+          :app_name => app_name,
+          :user => node[:owner_name],
+          :worker_name => "collector#{count+1}",
+          :framework_env => node[:environment][:framework_env]
+        })
+      end
+    end
+
+    execute "monit-reload-restart" do
+       command "sleep 30 && monit reload"
+       action :run
+    end
   end
 
 end
--- cookbooks/copier/recipes/default.rb
+++ cookbooks/copier/recipes/default.rb
@@ -1,4 +1,16 @@
 if node[:instance_role] =~ /^app/
+  template "/home/deploy/.pgpass" do
+    owner 'deploy'
+    group 'deploy'
+    mode 0600
+    source "pgpass.erb"
+    variables({
+      :hostname => node[:db_host],
+      :dbuser => node[:users].first[:username],
+      :dbpass => node[:users].first[:password]
+    })
+  end
+
   remote_file "/engineyard/bin/copier" do
     source "copier"
     owner "root"
